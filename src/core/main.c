262b33582d0fd6572bae89a52b4e5e6219cc546c
reza-ya-57 <rezaayaari.57@gmail.com>
2024-03-04 10:45:54 +0330

    work on structs :)

--- src/core/main.c
+++ src/core/main.c
@@ -1,4 +1,8 @@
 #include "stdio.h"
+#include "math.h"
+#include "stdlib.h"
+#include "string.h"
+
 #include "random-array.h"
 
 
@@ -11,12 +15,42 @@
 
 int main() {
 
-    struct ArrayInfo array_info;
-    array_info = random_array(10);
+    int number = -3;
+    int result;
+    double base , power , power_result;
+    base = 5;
+    char* str;
 
-    for(int i = 0; i < array_info.size; i++) {
-        print(array_info.array[i]);
+    for(int i = 0; i < 20; i++) {
+        str[i] = 'T';
     }
+
+    int test = printf("%d" , number);
+    printf("%d" , test);
+    // sprintf(str, "%d",number);
+    // int test = sizeof(str);
+    // int counter = 0;
+    // for(int i = 0; i < 20; i++) {
+    //     if (str[i] != 'T' && str[i] != '-') {
+    //         counter++;
+    //     }
+    // }
+    // printf("%d\n" , counter);
+    // power = counter - 1;
+
+    // printf("%f\n" , power);
+
+    // power_result = pow(base , power);   
+
+    // result = ((int)power_result) * number;
+    // printf("%d" , result);
+
+    // struct ArrayInfo array_info;
+    // array_info = random_array(10);
+
+    // for(int i = 0; i < array_info.size; i++) {
+    //     print(array_info.array[i]);
+    // }
 
     return 0;
 }
